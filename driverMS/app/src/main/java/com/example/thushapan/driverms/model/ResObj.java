61dfaf7f08c82b00d578fdb277625cd4577fcdb0
thushapan1996@gmail.com <thushapan1996@gmail.com>
2020-04-08 09:06:23 +0530

    initial commit

---
+++ driverMS/app/src/main/java/com/example/thushapan/driverms/model/ResObj.java
@@ -0,0 +1,8 @@
+package com.example.thushapan.driverms.model;
+
+/**
+ * Created by Thushapan on 8/21/19.
+ */
+
+public class ResObj {
+}
---
+++ driverMS/app/src/main/java/com/example/thushapan/driverms/remote/RetrofitClient.java
@@ -0,0 +1,8 @@
+package com.example.thushapan.driverms.remote;
+
+/**
+ * Created by Thushapan on 8/20/19.
+ */
+
+public class RetrofitClient {
+}
